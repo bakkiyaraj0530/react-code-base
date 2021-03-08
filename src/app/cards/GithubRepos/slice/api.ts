530fa3d5b591190a9ada6c1ed2f8ad302bb419f3
Bakkiyaraj Manokaran <bakkiyaraj@Bakkiyaraj-Mac.fritz.box>
2021-03-08 13:45:53 +0100

    Added new github token

--- src/app/cards/GithubRepos/slice/api.ts
+++ src/app/cards/GithubRepos/slice/api.ts
@@ -7,7 +7,7 @@ export const fetchData = (reponame: String | null) =>
   request(URL, {
     method: 'POST',
     headers: {
-      Authorization: 'Bearer 8d946bf3c1b5e14a3d7d7d12af16186c56ab4670',
+      Authorization: 'Bearer b096b1908d900c36767fe47eee505bb7b9db76d5',
     },
     body: JSON.stringify(
       reponame ? queryFetchRepoByName(reponame) : queryfetchRepos(),
